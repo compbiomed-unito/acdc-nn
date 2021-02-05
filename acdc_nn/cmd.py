3db7dfcc9698650d4f35793faad97ffdb0d3834f
Giovanni Birolo <giovanni.birolo@unito.it>
2021-02-05 17:49:54 +0100

    Fixed default weights loading

--- acdc_nn/cmd.py
+++ acdc_nn/cmd.py
@@ -14,7 +14,7 @@ import pandas as pd
 from pkg_resources import resource_filename
 
 def main():
-	print(resource_filename('acdc_nn', 'weights/Pesi_ACDCNN_postTL'))
+
 	parser = argparse.ArgumentParser(description='Predict the DDG of one or more mutations.')
 	# use subcommands
 	# https://docs.python.org/3/library/argparse.html#sub-commands
@@ -35,13 +35,13 @@ def main():
 
 	#if len(sys.argv)!= 5 and len(sys.argv)!=9:
 
-	#	print ("\n usage with 1 structure: mut prof pdb chain  \n usage with 2 structures:  mut  prof_w pdb_w chain_w mut_inv prof_m pdb_m chain_m")
+	#	print ("\n usage with 1 structure: mut prof pdb chain \n usage with 2 structures: mut prof_w pdb_w chain_w mut_inv prof_m pdb_m chain_m")
 	#	sys.exit()
 
 	
 	# information processing
 	# get structure
-	structure, pchain, seq, d_seq2pdb, d_pdb2seq  = util.pdb2info(args.structure, args.chain)
+	structure, pchain, seq, d_seq2pdb, d_pdb2seq = util.pdb2info(args.structure, args.chain)
 	prof = util.getProfile(args.profile)
 
 	# FIXME mutation dependent
@@ -70,7 +70,7 @@ def main():
 		
 		# information processing
 		# get structure
-		structure_m, pchain_m, seq_m, d_seq2pdb_m, d_pdb2seq  = util.pdb2info(pdb_path_m, chain_m)
+		structure_m, pchain_m, seq_m, d_seq2pdb_m, d_pdb2seq = util.pdb2info(pdb_path_m, chain_m)
 		prof_m = util.getProfile(prof_path_m)
 
 		kvar_m=(mut_inv[0],d_pdb2seq[mut_inv[1:-1]],mut_inv[-1])
@@ -78,7 +78,7 @@ def main():
 		
 		dist_neigh_3d_m= util.get_neigh_ps(kvar_pdb_m,5,d_seq2pdb_m,pchain_m) 
 		list_dist_neigh_3d_m = dist_neigh_3d_m[kvar_m]
-	  
+
 		# extracting features
 
 		codif_m=util.getMutCod(mut_inv)
@@ -90,19 +90,20 @@ def main():
 	else:
 	#if len(sys.argv)==5:
 		To_predict_inv=To_predict_dir.copy()
-		To_predict_inv.iloc[:,:20]=To_predict_inv.iloc[:,:20].replace([1.0,-1.0],[-1.0,1.0])	  
+		To_predict_inv.iloc[:,:20]=To_predict_inv.iloc[:,:20].replace([1.0,-1.0],[-1.0,1.0])
 
 	
 	# ACDN-NN building
 	# Making input in the proper shape 
 
 	Xm_d, X1D_d, X3D_d = nn.mkInp(np.asarray(To_predict_dir).astype(np.float32), 500)
-	Xm_i, X1D_i, X3D_i = nn.mkInp(np.asarray(To_predict_inv).astype(np.float32), 500)  
-  
+	Xm_i, X1D_i, X3D_i = nn.mkInp(np.asarray(To_predict_inv).astype(np.float32), 500)
+
 	num_H=[32,16]
 	d=0.2
 	model1, model2 = nn.ACDC(num_H,d,25)
-	model1.load_weights(path_weights)
+	weight_path = resource_filename('acdc_nn', 'weights/full_dataset_TL')
+	model1.load_weights(weight_path)
 	#model1.summary()
 	prediction=model1.predict([X3D_d, X1D_d, Xm_d , X3D_i, X1D_i, Xm_i])
 	print(prediction[0][0][0])		
--- tests/test.sh
+++ tests/test.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-python3 -m acdc-nn Q104H tests/profiles/2ocjA.prof tests/structures/2ocj.pdb A
-acdc-nn Q104H  profiles/2ocjA.prof structures/2ocj.pdb  A
-echo "Expected: 0.1735"
-acdc-nn V51I  profiles/1bsaA.prof structures/1bsa.pdb A  I51V profiles/1bniA.prof structures/1bni.pdb A
-echo "Expected: 0.6103"
+echo acdc-nn Q104H profiles/2ocjA.prof structures/2ocj.pdb A
+acdc-nn Q104H profiles/2ocjA.prof structures/2ocj.pdb A
+echo acdc-nn V51I profiles/1bsaA.prof structures/1bsa.pdb A
+acdc-nn V51I profiles/1bsaA.prof structures/1bsa.pdb A
+echo acdc-nn V51I profiles/1bsaA.prof structures/1bsa.pdb A --inverse I51V profiles/1bniA.prof structures/1bni.pdb A
+acdc-nn V51I profiles/1bsaA.prof structures/1bsa.pdb A --inverse I51V profiles/1bniA.prof structures/1bni.pdb A
 
