9a6f1cbe6db82254838d06153a3e055819e1f77e
Giovanni Birolo <giovanni.birolo@unito.it>
2021-02-08 16:04:45 +0100

    Rewrote acdc-nn command interface

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,9 @@
 FROM gitlab.c3s.unito.it:5000/gbirolo/pydev
-#FROM python:3.6-slim-buster
 
-#python3 -m pip install --index-url https://test.pypi.org/simple/ --no-deps acdc-nn
 COPY dist/acdc_nn-0.0.4-py3-none-any.whl /
 RUN python3 -m pip install acdc_nn-0.0.4-py3-none-any.whl
+#
+#FROM python:3.6-slim-buster
+#python3 -m pip install --index-url https://test.pypi.org/simple/ --no-deps acdc-nn
 
 ENTRYPOINT bash tests/test.sh
--- acdc_nn/cmd.py
+++ acdc_nn/cmd.py
@@ -1,113 +1,101 @@
 import sys
 import argparse
 
-
-
 from acdc_nn import util
 from acdc_nn import nn
 
 import numpy as np
 import pandas as pd
 
-#path_weights="weights/Pesi_ACDCNN_postTL"
-#/usr/local/lib/python3.8/site-packages/acdc_nn
 from pkg_resources import resource_filename
 
-def main():
 
-	parser = argparse.ArgumentParser(description='Predict the DDG of one or more mutations.')
-	# use subcommands
-	# https://docs.python.org/3/library/argparse.html#sub-commands
-	
-	#parser.add_argument('profile', metavar='PROF', type=argparse.FileType('r'), help='')
-	#parser.add_argument('structure', metavar='PDB', type=argparse.FileType('r'), help='')
-	parser.add_argument('mutation', metavar='MUT', help='mutation in the form Q339N') # FIXME better help
-	parser.add_argument('profile', metavar='PROF', help='')
-	parser.add_argument('structure', metavar='PDB', help='')
-	parser.add_argument('chain', metavar='CHAIN', help='')
-
-	parser.add_argument('--inverse', nargs=4, help='')
-	#parser.add_argument('--from-file', type=argparse.FileType('r'), help='')
-	
-
-	args = parser.parse_args()
-	#print(args)
-
-	#if len(sys.argv)!= 5 and len(sys.argv)!=9:
-
-	#	print ("\n usage with 1 structure: mut prof pdb chain \n usage with 2 structures: mut prof_w pdb_w chain_w mut_inv prof_m pdb_m chain_m")
-	#	sys.exit()
-
-	
-	# information processing
-	# get structure
-	structure, pchain, seq, d_seq2pdb, d_pdb2seq = util.pdb2info(args.structure, args.chain)
-	prof = util.getProfile(args.profile)
-
-	# FIXME mutation dependent
-	mut = args.mutation
-	kvar = (mut[0], d_pdb2seq[mut[1:-1]], mut[-1])
-	kvar_pdb = (mut[0], mut[1:-1], mut[-1])
-
-	dist_neigh_3d= util.get_neigh_ps(kvar_pdb, 5, d_seq2pdb, pchain) 
-	list_dist_neigh_3d = dist_neigh_3d[kvar]
-
-	# extracting features
-	codif = util.getMutCod(mut)
-	all_profile = util.Unified_prof(kvar[1], prof,seq, list_dist_neigh_3d)
-	#all_data=[*codif,*all_profile,*np.zeros(600-len(all_profile))]
-	To_predict_dir = pd.DataFrame([*codif, *all_profile, *np.zeros(600 - len(all_profile))]).T
-
-	# if clauses for the second structure
-
-	if args.inverse:
-	#if len(sys.argv)==9:
-		mut_inv, prof_path_m, pdb_path_m, chain_m = args.inverse
-		#mut_inv=sys.argv[5]
-		#prof_path_m = sys.argv[6]
-		#pdb_path_m = sys.argv[7]
-		#chain_m = sys.argv[8]
-		
-		# information processing
-		# get structure
-		structure_m, pchain_m, seq_m, d_seq2pdb_m, d_pdb2seq = util.pdb2info(pdb_path_m, chain_m)
-		prof_m = util.getProfile(prof_path_m)
-
-		kvar_m=(mut_inv[0],d_pdb2seq[mut_inv[1:-1]],mut_inv[-1])
-		kvar_pdb_m=(mut_inv[0],mut_inv[1:-1],mut_inv[-1])
-		
-		dist_neigh_3d_m= util.get_neigh_ps(kvar_pdb_m,5,d_seq2pdb_m,pchain_m) 
-		list_dist_neigh_3d_m = dist_neigh_3d_m[kvar_m]
-
-		# extracting features
-
-		codif_m=util.getMutCod(mut_inv)
-		all_profile_m = util.Unified_prof(kvar_m[1],prof_m,seq_m, list_dist_neigh_3d_m)
-		To_predict_inv=pd.DataFrame([*codif_m,*all_profile_m,*np.zeros(600-len(all_profile_m))]).T
-
-	 
-	# ACDN-NN 1-input
-	else:
-	#if len(sys.argv)==5:
-		To_predict_inv=To_predict_dir.copy()
-		To_predict_inv.iloc[:,:20]=To_predict_inv.iloc[:,:20].replace([1.0,-1.0],[-1.0,1.0])
-
-	
-	# ACDN-NN building
-	# Making input in the proper shape 
-
-	Xm_d, X1D_d, X3D_d = nn.mkInp(np.asarray(To_predict_dir).astype(np.float32), 500)
-	Xm_i, X1D_i, X3D_i = nn.mkInp(np.asarray(To_predict_inv).astype(np.float32), 500)
-
+def load_nn(weight_path):
 	num_H=[32,16]
 	d=0.2
 	model1, model2 = nn.ACDC(num_H,d,25)
 	weight_path = resource_filename('acdc_nn', 'weights/full_dataset_TL')
 	model1.load_weights(weight_path)
-	#model1.summary()
-	prediction=model1.predict([X3D_d, X1D_d, Xm_d , X3D_i, X1D_i, Xm_i])
-	print(prediction[0][0][0])		
+	return model1, model2
 
+def	load_prot(profile_path, pdb_path, chain):
+	r = dict(zip(['structure', 'pchain', 'seq', 'd_seq2pdb', 'd_pdb2seq'], util.pdb2info(pdb_path, chain)))
+	r['prof'] = util.getProfile(profile_path)
+	return r
+
+def compute_nn_input(mut, prot):
+	# FIXME mutation dependent
+	kvar = (mut[0], prot['d_pdb2seq'][mut[1:-1]], mut[-1])
+	kvar_pdb = (mut[0], mut[1:-1], mut[-1])
+
+	dist_neigh_3d= util.get_neigh_ps(kvar_pdb, 5, prot['d_seq2pdb'], prot['pchain']) 
+	list_dist_neigh_3d = dist_neigh_3d[kvar]
+
+	# extracting features
+	codif = util.getMutCod(mut)
+	all_profile = util.Unified_prof(kvar[1], prot['prof'], prot['seq'], list_dist_neigh_3d)
+	#all_data=[*codif,*all_profile,*np.zeros(600-len(all_profile))]
+	to_predict_dir = pd.DataFrame([*codif, *all_profile, *np.zeros(600 - len(all_profile))]).T
+	return to_predict_dir
+
+
+
+def main():
+	parser = argparse.ArgumentParser(description='Predict the DDG of one or more mutations.')
+	parser.add_argument('--output', metavar='OUTFILE', type=argparse.FileType('w'), default=sys.stdout, help='output file path, use stdout otherwise')
+	parser.add_argument('--weights', metavar='NN-WEIGHTS', 
+		default=resource_filename('acdc_nn', 'weights/full_dataset_TL'), 
+		help='path to the network weights, in tensorflow format') # FIXME what is the name of the format?
+	subparsers = parser.add_subparsers(dest='command', metavar='CMD', required=True)
+
+	single_parser = subparsers.add_parser('single', help='predict a single mutation')
+	single_parser.add_argument('wt_mutation', metavar='MUT', help='mutation in the form Q339N')
+	single_parser.add_argument('wt_profile', metavar='PROF', help='')
+	single_parser.add_argument('wt_structure', metavar='PDB', help='')
+	single_parser.add_argument('wt_chain', metavar='CHAIN', help='')
+
+	inverse_parser = subparsers.add_parser('inverse', help='predict a single mutation using also the inverse structure')
+	inverse_parser.add_argument('wt_mutation', metavar='MUT', help='mutation in the form Q339N')
+	inverse_parser.add_argument('wt_profile', metavar='WT-PROF', help='')
+	inverse_parser.add_argument('wt_structure', metavar='WT-PDB', help='')
+	inverse_parser.add_argument('wt_chain', metavar='WT-CHAIN', help='')
+	inverse_parser.add_argument('mt_mutation', metavar='INV-MUT', help='inverse mutation in the form Q339N')
+	inverse_parser.add_argument('mt_profile', metavar='MT-PROF', help='')
+	inverse_parser.add_argument('mt_structure', metavar='MT-PDB', help='')
+	inverse_parser.add_argument('mt_chain', metavar='MT-CHAIN', help='')
+
+	batch_parser = subparsers.add_parser('batch', help='predict a list of mutations from a file')
+	batch_parser.add_argument('mutations', metavar='MUT-FILE', type=argparse.FileType('r'))
+
+	#profile_parser = subparsers.add_parser('profile', help='compute the profile from multiple alignment file') # we could also simply accept a multialn file and make a profile
+	#profile_parser.add_argument('multialn', metavar='ALN-FILE', type=argparse.FileType('r'))
+
+	args = parser.parse_args()
+	
+	if args.command in {'single', 'inverse'}:
+		# ACDN-NN building
+		model1, model2 = load_nn(args.weights)
+
+		# compute input features for the direct mutation using the wild-type protein
+		wt_prot = load_prot(args.wt_profile, args.wt_structure, args.wt_chain)
+		to_predict_dir = compute_nn_input(args.wt_mutation, wt_prot)
+		if args.command == 'inverse': # compute input features for the inverse mutation using the mutated protein
+			mt_prot = load_prot(args.mt_profile, args.mt_structure, args.mt_chain)
+			to_predict_inv = compute_nn_input(args.mt_mutation, mt_prot)
+		else: # compute input features for the inverse mutation inverting the input features for the direct mutation
+			to_predict_inv = to_predict_dir.copy()
+			to_predict_inv.iloc[:, :20] = to_predict_inv.iloc[:, :20].replace([1.0, -1.0],[-1.0, 1.0])
+	
+		# Making input in the proper shape 
+		Xm_d, X1D_d, X3D_d = nn.mkInp(np.asarray(to_predict_dir).astype(np.float32), 500)
+		Xm_i, X1D_i, X3D_i = nn.mkInp(np.asarray(to_predict_inv).astype(np.float32), 500)
+
+		prediction=model1.predict([X3D_d, X1D_d, Xm_d , X3D_i, X1D_i, Xm_i])
+		print(prediction[0][0][0], file=args.output)
+	#elif args.command == 'batch':
+	#elif args.command == 'profile':
+	else:
+		raise NotImplementedError('Command {} not implemented'.format(args.command))
 
 if __name__ == '__main__':
 	main()
